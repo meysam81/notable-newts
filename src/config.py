9b4ecc661a0a376ed9be8b59ec3d4bcb47445ccb
Balint Szilagyi <s2064308@ed.ac.uk>
2021-07-14 09:30:09 +0100

    saving implemented
    it's having issues with pickle

--- src/config.py
+++ src/config.py
@@ -9,5 +9,9 @@ root_dir = Path(os.path.dirname(__file__)).parent
 class GeneralSettings(BaseSettings):
     GAME_LOGO_PATH = (root_dir / "docs/images/game-avatar.png").absolute()
 
+    def new_maze_path(self):
+        nr_files = len(os.listdir(f"{root_dir}/docs/mazes"))
+        return (root_dir / f"docs/mazes/maze_{nr_files}.pickle").absolute()
+
 
 general_settings = GeneralSettings()
--- src/maze_generator.py
+++ src/maze_generator.py
@@ -1,11 +1,14 @@
+# import pickle  # nosec
+
 import pygame
+
+# import config
 
 pygame.init()
 
 
 class Tile:
     """The tiles that levels are based on"""
-
     def __init__(self, passable: bool = True, enemy: bool = False):
         # we could have different terrains like water to spice up the level design
         self.passable = passable
@@ -21,12 +24,32 @@ grid = [[Tile() for i in range(9)] for j in range(9)]
 
 def select_square():
     selected = True
-    square_pos_list = []
-    while selected:
+    selected_tool = add_obstacle
+    while True:
+        square_pos_list = []
+        canvas.fill((40, 40, 40))
+        draw()
+        pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    print("add selected")
+                    selected_tool = add_obstacle
+                if event.key == pygame.K_d:
+                    print("delete selected")
+                    selected_tool = remove
+                if event.key == pygame.K_e:
+                    print("enemy selected")
+                    selected_tool = add_enemy
+                if event.key == pygame.K_s:
+                    print("saving")
+                    # with open(config.general_settings.new_maze_path(), "wb") as f:
+                    # pickle.dump(grid, f)
+                    # f.close()
+
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 while selected:
                     for event in pygame.event.get():
@@ -34,71 +57,56 @@ def select_square():
                             pygame.quit()
                             exit()
                         x_pos, y_pos = pygame.mouse.get_pos()
-                        pygame.draw.rect(
-                            canvas, (255, 0, 0),
-                            ((x_pos//s_s)*s_s, (y_pos//s_s)*s_s, s_s, s_s))
+                        if selected_tool == add_obstacle:
+                            pygame.draw.rect(
+                                canvas, (200, 200, 200),
+                                ((x_pos//s_s)*s_s, (y_pos//s_s)*s_s, s_s, s_s))
+                        if selected_tool == remove:
+                            pygame.draw.rect(
+                                canvas, (0, 0, 0),
+                                ((x_pos//s_s)*s_s, (y_pos//s_s)*s_s, s_s, s_s))
+                        if selected_tool == add_enemy:
+                            pygame.draw.rect(
+                                canvas, (200, 50, 50),
+                                ((x_pos//s_s)*s_s, (y_pos//s_s)*s_s, s_s, s_s))
+
                         pygame.display.update()
                         pos = (y_pos//s_s, x_pos//s_s)
                         if pos not in square_pos_list:
                             square_pos_list.append(pos)
 
                         if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                            for sqr in square_pos_list:
+                                selected_tool(sqr)
                             selected = False
-    return square_pos_list
+
+        if pygame.mouse.get_pressed()[2]:
+            break
+        selected = True
 
 
 def add_obstacle(pos):
     grid[pos[0]][pos[1]].passable = False
 
 
-def remove_obstacle(pos):
+def remove(pos):
     grid[pos[0]][pos[1]].passable = True
+    grid[pos[0]][pos[1]].enemy = False
 
 
 def add_enemy(pos):
     grid[pos[0]][pos[1]].enemy = True
 
 
-def select_tool():
-    selected_tool = add_obstacle
-    print("\nselect tool:")
-    while 1:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-        if pygame.key.get_pressed()[pygame.K_UP]:
-            print("add selected")
-            selected_tool = add_obstacle
-            break
-        if pygame.key.get_pressed()[pygame.K_DOWN]:
-            print("delete selected")
-            selected_tool = remove_obstacle
-            break
-        if pygame.key.get_pressed()[pygame.K_LEFT]:
-            print("enemy selected")
-            selected_tool = add_enemy
-            break
-    return selected_tool
-
-
-def draw_stage():
-    x, y = 0, 0
-    for row in grid:
-        for column in row:
-            pygame.draw.rect(canvas, (10, 10, 10), (x, y, s_s, s_s), 1)
-            x += s_s
-        y += s_s
-        x = 0
-
-
 def draw():
     x, y = 0, 0
     for row in range(len(grid)):
         for col in range(len(grid[row])):
-            if grid[row][col].enemy:
-                pygame.draw.rect(canvas, (100, 10, 10), (x, y, s_s, s_s), 1)
-            if grid[row][col].passable:
+            if grid[row][col].enemy and not grid[row][col].passable:
+                pygame.draw.rect(canvas, (100, 70, 70), (x, y, s_s, s_s), 1)
+            elif grid[row][col].enemy and grid[row][col].passable:
+                pygame.draw.rect(canvas, (200, 50, 50), (x, y, s_s, s_s), 1)
+            elif grid[row][col].passable:
                 pygame.draw.rect(canvas, (10, 10, 10), (x, y, s_s, s_s), 1)
             else:
                 pygame.draw.rect(canvas, (200, 200, 200), (x, y, s_s, s_s), 1)
@@ -107,24 +115,9 @@ def draw():
         x = 0
 
 
-# 0: add
-# 1: delete
-# 2: enemy
+# a: add
+# d: delete
+# e: enemy
+# s: saving
 
-
-while 1:
-    canvas.fill((50, 50, 50))
-    draw()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-
-    # draw_stage()
-    pygame.display.update()
-    # i am reusing the square selection function and
-    # because it's not running in the main loop, you can't change tools real time
-    selected_tool = select_tool()
-    sqrs = select_square()
-    for sqr in sqrs:
-        selected_tool(sqr)
+select_square()
