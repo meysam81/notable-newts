c8c45f4347287fd4c18df1896279e4616b5ee409
Balint Szilagyi <s2064308@ed.ac.uk>
2021-07-16 15:20:00 +0100

    Feature/maze generator (#15)
    * basic maze generator
    
    fundamentals are made
    the code is rushed and not nice, i know
    
    all that's left functionally is to add a saving feature which is just dumping the grid array to a pickle file
    
    * saving implemented
    
    it's having issues with pickle
    
    * adding back pickle
    
    * Update config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 import os
-from pathlib import Path
+from pathlib import Path, WindowsPath
 
 from pydantic import BaseSettings
 
@@ -8,6 +8,13 @@ root_dir = Path(os.path.dirname(__file__)).parent
 
 class GeneralSettings(BaseSettings):
     GAME_LOGO_PATH = (root_dir / "docs/images/game-avatar.png").absolute()
+
+    def new_maze_path(self) -> WindowsPath:
+        nr_files = len(os.listdir(f"{root_dir}/docs/mazes"))
+        return (root_dir / f"docs/mazes/{nr_files}.pickle").absolute()
+
+    def load_maze(self, nr):
+        return (root_dir / f"docs/mazes/{nr}.pickle").absolute()
 
 
 general_settings = GeneralSettings()
